e7ace0acc22663f11f77aeea2f67b68fbe644308
Кирилл Бизюков <bizukov@reg.ru>
2019-02-21 18:51:03 +0300

    add config

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Browserslist Shared Config
+ * https://github.com/ai/browserslist#shareable-configs
+ *
+ * Instead of using a `.browserslistrc` or `browserslist`
+ * config file, you can reference a shared config file.
+ *
+ * Use in your package.json as:
+ *
+ *  "browserslist": [
+ *    "extends browserslist-shareable-config"
+ *  ]
+ *
+ */
+
+module.exports = [
+	"> 1% in RU",
+    "Chrome >= 50",
+    "Firefox >= 45",
+    "Explorer >= 11",
+    "Safari >= 9",
+    "iOS >= 9",
+	"Edge >= 12",
+	"current node"
+];
